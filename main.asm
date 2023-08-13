9bda1c74a2bf37c754cf136742fb38ac393c50fb
John Connolly <contact@connosoft.com>
2023-08-13 13:41:40 +0100

    New row list display driver added

--- main.asm
+++ main.asm
@@ -54,38 +54,46 @@ gameloop:
         jr gameloop
 
 wrxDriver:
-; Total time from display driver entry to first display byte must be 174 T-states
+; Total time from display driver entry to first display byte must be
+; 174 T-states.
 ; Code from label wrxLoop to first display byte takes 56 T-states
 ; Tasks to carry out during initialisation include:
 ;   - Disable interrupts
 ;   - Load appropriate registers
-        di                              ; [ 4]
-        ld a,$40                        ; [ 7]
-        ld i,a                          ; [ 9]
+        di                              ; [ 4] WRX needs interrupts off
+        ld a,$40                        ; [ 7] 
+        ld i,a                          ; [ 9] Point to RAM for WRX display
 
         ld a,$c9                        ; [ 7] Opcode for RET
         ld (displayRoutine + 34),a      ; [13] Set up correct exit from hi-res
 
-        ld b,5
-        djnz $
-        nop
-        
-        ld b,192 - 8                    ; [ 7] 184 lines for hi-res section        
+        ld hl,rowTab                    ; [10] Load row table pointer
+
+                                        ;  50  T-STATES so far
+
+        ld b,4                          ; [ 7]
+        djnz $                          ; [47]
+        nop                             ; [ 4] --- DELAY ---
+
+        ld bc,$b8aa                     ; [10] B holds number of rows (184)
+                                        ;      C holds dither offset pattern
 
 wrxLoop:
-        ld a,0                          ; [ 7]
-        ld a,b                          ; [ 4]
-        rrca                            ; [ 4]
-        rrca                            ; [ 4]
-        rrca                            ; [ 4]
-        and $60                         ; [ 7]
+        rlc c                           ; [ 8] Get dither bit in carry
+        ld a,(hl)                       ; [ 7] Get row index from table
+        rra                             ; [ 4] Stick dither bit in MSB
+        inc hl                          ; [ 6] Move pointer to next index
+        ret c                           ; [ 5] --- DELAY ---
 
         call displayRoutine + $8000     ; [17] Call the WRX display routine
-                                        ; 147 T-states spent in displayRoutine
-        djnz wrxLoop                    ; [13]
+
+                                        ; 147  T-STATES spent in displayRoutine
+
+        djnz wrxLoop                    ; [13] Loop if there are rows left
 
 wrxText:
-; Total time from last hi-res display byte to Sinclair ROM driver call must be 155 T-states
+; Total time from last hi-res display byte to Sinclair ROM driver call must be
+; 155 T-states
 ; Time spent before this label = 18 T-states
 
         ld a,$76                        ; [ 7] Opcode for HALT
@@ -100,9 +108,9 @@ wrxText:
 
 ; Total for following set-up section = 60
         ld bc,$0108                     ; [10] 1 row, 8 lines
-        ld hl,displayRoutine + $8002    ; [10]
-        ld a,$1e                        ; [ 7] Point to our ROM character set
-        ld i,a                          ; [ 9]
+        ld hl,displayRoutine + $8002    ; [10] Point to our text line
+        ld a,$1e                        ; [ 7] 
+        ld i,a                          ; [ 9] Point to our ROM character set
         ld a,$f5                        ; [ 7] SLOW mode timing value for R
         call $2b5                       ; [17] Generate the text with the ROM display driver
         call $292                       ; [17] Exit the display driver
@@ -119,6 +127,9 @@ displayRoutine:
         db $3f, $3d, $24, $1d, $1a, $00, $35, $37, $34, $38, $32, $00, $1e, $1c, $1e, $1f
 
         ret
+
+rowTab:
+        ds 184
 
 
 ; the display file, Code the lines needed.
